c189d1a77ca186395f1691eb7b37e6f74f373dc1
Kohan5 <emrailatuconsina@gmail.com>
2022-09-22 11:41:46 +0700

    perbaikan html 5

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -8,7 +8,6 @@ from mywatchlist.views import show_json
 app_name = 'mywatchlist'
 
 urlpatterns = [
-    path('', show_mywatchlist, name='show_mywatchlist'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('html/', show_mywatchlist, name='show_mywatchlist')
